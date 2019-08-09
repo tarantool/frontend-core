e57bb8abfaeeefc9da93cee9265f07d61da346a3
Tatyana M Gulina <tt@hlobys.ru>
2019-08-09 11:37:23 +0300

    Create a webpack plugin for lua bundle

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -9,6 +9,9 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 
+const LuaBundlePlugin = require('../packages/LuaBundlerWebpackPlugin');
+
+
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
@@ -263,6 +266,9 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+    new LuaBundlePlugin()
+
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
---
+++ packages/LuaBundlerWebpackPlugin/index.js
@@ -0,0 +1,52 @@
+const mime = require('mime-types');
+const path = require('path');
+const mainFs = require('fs');
+const cp = require('child_process');
+const debug = require('debug')('LuaBundlePlugin');
+
+const walkSync = function(dir, filelist) {
+  const files = mainFs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(file => {
+    if (mainFs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    } else {
+      filelist.push(dir + file);
+    }
+  });
+  return filelist;
+};
+
+
+class LuaBundlePlugin {
+  constructor(options) {
+    this.options = options || {}
+
+  }
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('LuaBundlePlugin', compilation => {
+      const outputPath = compiler.options.output.path;
+      const buildFoler = path.relative(process.cwd(), outputPath);
+      const namespace = this.options.namespace || '';
+      const namespaceFolder = buildFoler + '/static/' + (namespace ? namespace + '/' : '');
+      const files = walkSync(namespaceFolder);
+      const filemap = {};
+      const isEntry = this.options.entryRegExp || /main.+js$/;
+      for (const file of files){
+        const fileName = file.slice(namespaceFolder.length);
+        const fileBody = mainFs.readFileSync(file, { encoding: 'utf8' });
+        filemap[fileName] = {
+          is_entry: isEntry.test(fileName),
+          body: fileBody,
+          mime: mime.lookup(fileName)
+        }
+      }
+      mainFs.writeFileSync(buildFoler + '/bundle.json', JSON.stringify(filemap), { encoding: 'utf8' })
+      debug('compile bundle.json')
+      cp.execSync('tarantool -l pack-front - build/bundle.json build/bundle.lua')
+      debug('build bundle.lua')
+    });
+  }
+}
+
+module.exports = LuaBundlePlugin;
