a9cd05744b5cdd796c03a5e6badaf4c74bcb94fb
Roman Babaev <kotfly@gmail.com>
2019-09-19 19:08:12 +0300

    Add png and svg favicons (#21)

---
+++ src/components/FavIcon/index.js
@@ -0,0 +1,26 @@
+// @flow
+import { Component } from 'react';
+import icon from './favicon@32.png';
+import iconSvg from './favicon.svg';
+
+export default class FavIcon extends Component<any> {
+  componentDidMount () {
+    const svgIconElement = document.createElement('link');
+    svgIconElement.setAttribute('rel', 'shortcut icon');
+    svgIconElement.setAttribute('href', iconSvg);
+    svgIconElement.setAttribute('type', 'image/svg+xml');
+    svgIconElement.setAttribute('sizes', 'any');
+    document.head && document.head.appendChild(svgIconElement);
+
+    const pngIconElement = document.createElement('link');
+    pngIconElement.setAttribute('rel', 'shortcut icon');
+    pngIconElement.setAttribute('href', icon);
+    pngIconElement.setAttribute('type', 'image/png');
+    pngIconElement.setAttribute('sizes', '32x32');
+    document.head && document.head.appendChild(pngIconElement);
+  }
+
+  render () {
+    return null;
+  }
+}
