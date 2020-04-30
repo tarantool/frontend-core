f71bc1114c51b7ac04299bd836b28e5525c92aa2
Nikolay Mendyaev <n.mendyaev@corp.mail.ru>
2020-04-30 10:12:22 +0300

    fix: move nanoid package to repo

---
+++ src/integration.test.js
@@ -0,0 +1,8 @@
+require('../compiled_module/core')
+
+
+
+test('install', () => {
+  document.body.innerHTML = '<div id="root"></div>'
+  window.tarantool_enterprise_core.install()
+})
--- src/store/reducers/pageFilter.js
+++ src/store/reducers/pageFilter.js
@@ -2,17 +2,18 @@
 
 import type { FSA } from '../../core'
 import { disposableFunctionKey } from '../../utils/disposableFnMap'
-import * as R from 'ramda'
 import { PAGE_FILTER_ADD, PAGE_FILTER_REMOVE } from '../constants'
 
 export type PageFilterState = Array<string>
 
-export const initialState: PageFilterState = [disposableFunctionKey(R.T)]
+const defaultFnKey: string = disposableFunctionKey(() => true)
+
+export const initialState: PageFilterState = [defaultFnKey]
 
 export default (state: PageFilterState = initialState, action: FSA): PageFilterState => {
   switch (action.type) {
     case PAGE_FILTER_ADD: {
-      if (action.payload && action.payload.fn) return state.concat(action.payload.fn)
+      if (action.payload && typeof action.payload === 'object' && action.payload.fn && typeof action.payload.fn === 'string') return state.concat(action.payload.fn)
       else return state
     }
     case PAGE_FILTER_REMOVE: {
